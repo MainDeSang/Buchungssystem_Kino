cb406b1e7ed6911b177bdf723a54436a8130b149
JeNesSaisPas <jenessaispasouplus@gmail.com>
2024-11-13 10:37:51 +0100

    makes a User

---
+++ src/User.java
@@ -0,0 +1,16 @@
+public class User {
+   private String name;
+   private String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName(String name) {
+        return name;
+    }
+    public String getPassword(String password) {
+       return password;
+    }
+}
